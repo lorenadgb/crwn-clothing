992edcb47c7ea2bd5e54a4f7937d5c63e2262963
lorenadgb <lorena@vyans.com>
2020-03-11 22:01:00 +1100

    Update app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
+import ShopPage from "./components/shop/shop.component";
+
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <HomePage/>
+        <Switch>
+            <Route exact path={'/'} component={HomePage} />
+            <Route exact path={'/shop'} component={ShopPage} />
+        </Switch>
     </div>
   );
 }
